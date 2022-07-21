4f252483e8954e7a8d8f45a3dda0162ba82b8966
Dylan <dylancarver14@gmail.com>
2022-07-20 23:16:13 -0500

    commit tree bb84b8640648e2f908091bf82460149f8cf1aab3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+//const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(drivers) {
+        return (drivers.slice(0, 2))
+    }
+
+    const returnLastTwoDrivers = function(drivers) {
+        return (drivers.slice(2, 4))
+    }
+
+    //selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+    const selectingDrivers = function(drivers) {
+        return returnFirstTwoDrivers.drivers.slice(0,2)
+    }
